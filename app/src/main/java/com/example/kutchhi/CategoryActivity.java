2db726c55f0c9f7833e167f895a2b4a3ca7892e8
Manan Karani <manankarani@gmail.com>
2020-07-03 16:14:02 +0530

    Added FramgmentNumbers, FragmentPhrases, FragmentFamily

--- app/src/main/java/com/example/kutchhi/CategoryActivity.java
+++ app/src/main/java/com/example/kutchhi/CategoryActivity.java
@@ -8,23 +8,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_category);
-        CardView card_view = (CardView) findViewById(R.id.card_view_colors); // creating a CardView and assigning a value.
+        CardView colorsCard = (CardView) findViewById(R.id.card_view_colors); // creating a CardView and assigning a value.
+        CardView numbersCard = (CardView) findViewById(R.id.card_view_numbers);
+        CardView phrasesCard = (CardView) findViewById(R.id.card_view_phrases);
+        CardView familyCard = (CardView) findViewById(R.id.card_view_family);
+        colorsCard.setOnClickListener(this);
+        numbersCard.setOnClickListener(this);
+        familyCard.setOnClickListener(this);
+        phrasesCard.setOnClickListener(this);
 
-        card_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // do whatever you want to do on click (to launch any fragment or activity you need to put intent here.)
-                Intent mainActivity = new Intent(getApplicationContext(),  HolderActivity.class);
-                startActivity(mainActivity);
-
-            }
-        });
     }
+    @Override
+    public void onClick(View v) {
+        Intent mainActivity = new Intent(getApplicationContext(),  HolderActivity.class);
+        startActivity(mainActivity);
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/kutchhi/FragmentNumbers.java
@@ -0,0 +1,50 @@
+package com.example.kutchhi;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class FragmentNumbers extends Fragment {
+
+
+    public FragmentNumbers() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutti", R.drawable.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight));
+        words.add(new Word("nine", "wo’e", R.drawable.number_nine));
+        words.add(new Word("ten", "na’aacha", R.drawable.number_ten));
+        WordAdapter itemsAdapter = new WordAdapter(getActivity(), words);
+        ListView listView = (ListView) rootView.findViewById(R.id.wordlist);
+        listView.setAdapter(itemsAdapter);
+        return rootView;
+    }
+}
